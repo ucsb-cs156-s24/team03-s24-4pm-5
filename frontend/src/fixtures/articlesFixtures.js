df42b47de7b65a5fae4656ae4a1bf4001cc15182
Garvin Young <garvinwyoung@ucsb.edu>
2024-05-01 19:00:19 -0700

    added articlesFixtures with oneArticle and threeArticles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const ucsbDatesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "how to cure cancer",
+        "url": "medicalwebsite.com/cancer",
+        "explanation": "this is how you cure cancer for real this time guys",
+        "email": "certifieddoctor@gmail.com",
+        "localDateTime": "2000-01-01T12:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "how to cure cancer",
+            "url": "medicalwebsite.com/cancer",
+            "explanation": "this is how you cure cancer for real this time guys",
+            "email": "certifieddoctor@gmail.com",
+            "localDateTime": "2000-01-01T12:00:00"
+        },
+        {
+            "id": 2,
+            "title": "you wont believe what THIS celebrity did",
+            "url": "tabloid.com/dailynews",
+            "explanation": "i dont know, they ate a pickle or something",
+            "email": "nolifepaparazzi@gmail.com",
+            "localDateTime": "4444-01-01T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "aliens invaded the the US",
+            "url": "USCoastGuard.com/home",
+            "explanation": "nvm false alarm, they were just canadians",
+            "email": "theUSGovernment@gmail.com",
+            "localDateTime": "1776-01-01T12:00:00"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
