a022646b70933652d6b8b66c9d9724ecc55eaabb
RayDu <97069739+CongruiDu@users.noreply.github.com>
2024-05-10 14:02:33 -0700

    Revert "RD- add create page for ucsb org"
    This reverts commit b8eca91b8c884452bf788cb034094f40d440e57f.

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.js
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.js
@@ -1,119 +1,43 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
+import { render, screen } from "@testing-library/react";
+import PlaceholderCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-const mockToast = jest.fn();
-jest.mock('react-toastify', () => {
-    const originalModule = jest.requireActual('react-toastify');
-    return {
-        __esModule: true,
-        ...originalModule,
-        toast: (x) => mockToast(x)
-    };
-});
-
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => {
-    const originalModule = jest.requireActual('react-router-dom');
-    return {
-        __esModule: true,
-        ...originalModule,
-        Navigate: (x) => { mockNavigate(x); return null; }
-    };
-});
-
-describe("UCSBOrganizationCreatePage tests", () => {
+describe("PlaceholderCreatePage tests", () => {
 
     const axiosMock = new AxiosMockAdapter(axios);
 
-    beforeEach(() => {
-        jest.clearAllMocks();
+    const setupUserOnly = () => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    });
+    };
 
     const queryClient = new QueryClient();
-    test("renders without crashing", () => {
+    test("Renders expected content", () => {
+        // arrange
+
+        setupUserOnly();
+       
+        // act
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <UCSBOrganizationCreatePage />
+                    <PlaceholderCreatePage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
+
+        // assert
+        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
     });
 
-    test("on submit, makes request to backend, and redirects to /UCSBOrganization", async () => {
-
-        const queryClient = new QueryClient();
-        const UCSBOrganization = {
-            orgCode: "CD",
-            orgTranslationShort: "Coder",
-            orgTranslation: "CoderSB",
-            inactive: false
-        };
-
-        axiosMock.onPost("/api/UCSBOrganization/post").reply(202, UCSBOrganization);
-
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <UCSBOrganizationCreatePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        )
-
-        await waitFor(() => {
-            expect(screen.getByLabelText("orgCode")).toBeInTheDocument();
-        });
-
-        const orgCodeInput = screen.getByLabelText("orgCode");
-        expect(orgCodeInput).toBeInTheDocument();
-
-        const orgTranslationShortInput = screen.getByLabelText("orgTranslationShort");
-        expect(orgTranslationShortInput).toBeInTheDocument();
-
-        const orgTranslationInput = screen.getByLabelText("orgTranslation");
-        expect(orgTranslationInput).toBeInTheDocument();
-
-        const inactiveInput = screen.getByLabelText("inactive");
-        expect(inactiveInput).toBeInTheDocument();
-
-
-        const createButton = screen.getByText("Create");
-        expect(createButton).toBeInTheDocument();
-
-        fireEvent.change(orgCodeInput, { target: { value: 'CD' } })
-
-        fireEvent.change(orgTranslationShortInput, { target: { value: 'Coder' } })
-        fireEvent.change(orgTranslationInput, { target: { value: "CoderSB" } })
-        fireEvent.change(inactiveInput, { target: { value: "false" } })
-
-        fireEvent.click(createButton);
-
-        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
-
-        expect(axiosMock.history.post[0].params).toEqual({
-            orgCode: "CD",
-            orgTranslationShort: "Coder",
-            orgTranslation: "CoderSB",
-            inactive: "false"
-        });
-
-        // assert - check that the toast was called with the expected message
-        expect(mockToast).toBeCalledWith("New UCSB Organization Created - orgCode: CD orgTranslationShort: Coder orgTranslation: CoderSB inactive: false");
-        expect(mockNavigate).toBeCalledWith({ "to": "/UCSBOrganization" });
-
-    });
 });
 
 
