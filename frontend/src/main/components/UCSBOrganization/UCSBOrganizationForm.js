af84bf2821a284691479b8a3fcffebd9e08c4057
RayDu <97069739+CongruiDu@users.noreply.github.com>
2024-05-09 17:29:34 -0700

    RD- small fix on org form

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.js
@@ -83,8 +83,8 @@ function UCSBOrganizationForm({ initialContents, submitAction, buttonLabel = "Cr
                         pattern: inactive_regex
                     })}
                 />
-                <Form.Control.Feedback type="invalid">
-                    {errors.inactive?.message && 'inactive is required. '}
+            <Form.Control.Feedback type="invalid">
+                    {errors.inactive?.message}
                 </Form.Control.Feedback>
             </Form.Group>
 
