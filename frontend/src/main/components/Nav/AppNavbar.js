e98927c43f4716c1582bfb0fcdb36ed0c0b01e6c
Baimin Wang <baiminwang@ucsb.edu>
2024-05-10 21:11:05 -0700

    Placeholder fix

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -60,6 +60,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <Nav.Link as={Link} to="/MenuItemReview">MenuItemReview</Nav.Link>
                   <Nav.Link as={Link} to="/recommendationrequest">Recommendation Request</Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
+                  <Nav.Link as={Link} to="/diningcommonsmenuitem">UCSBDiningCommonsMenuItem</Nav.Link>
                 </>
               )
             }
