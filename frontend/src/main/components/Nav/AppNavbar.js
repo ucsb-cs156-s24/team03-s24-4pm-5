614bb47eec364c6415577f9322bc6a34d2a3e6ef
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-05-13 14:52:04 -0700

    Update AppNavbar.js

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -62,6 +62,8 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
                   <Nav.Link as={Link} to="/articles">Articles</Nav.Link>
                   <Nav.Link as={Link} to="/UCSBOrganization">UCSB Organization</Nav.Link>
+                  <Nav.Link as={Link} to="/helprequest">HelpRequest</Nav.Link>
+
                 </>
               )
             }
