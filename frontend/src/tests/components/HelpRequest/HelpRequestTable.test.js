05fad13f51dbee4c81e73db4ab9de194fb97bdae
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-05-13 22:55:41 -0700

    Update HelpRequestTable.test.js

--- frontend/src/tests/components/HelpRequest/HelpRequestTable.test.js
+++ frontend/src/tests/components/HelpRequest/HelpRequestTable.test.js
@@ -115,7 +115,7 @@ describe("UserTable tests", () => {
 
     fireEvent.click(editButton);
 
-    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/helprequest/edit/1'));
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/HelpRequest/edit/1'));
 
   });
 
