f59a1fd5c7aaabadbc28ad04596b90d7535650d7
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-05-13 22:48:10 -0700

    Update HelpRequestTable.js

--- frontend/src/main/components/HelpRequest/HelpRequestTable.js
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.js
@@ -11,7 +11,7 @@ export default function HelpRequestTable({ helpRequests, currentUser }) {
     const navigate = useNavigate();
 
     const editCallback = (cell) => {
-        navigate(`/helprequest/edit/${cell.row.values.id}`)
+        navigate(`/HelpRequest/edit/${cell.row.values.id}`)
     }
 
     // Stryker disable all : hard to test for query caching
@@ -19,7 +19,7 @@ export default function HelpRequestTable({ helpRequests, currentUser }) {
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
         { onSuccess: onDeleteSuccess },
-        ["/api/helprequest/all"]
+        ["/api/HelpRequest/all"]
     );
     // Stryker restore all 
 
